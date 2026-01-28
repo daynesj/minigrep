0406380b1b2d5715e33dbbd86400e992179f792f
Jason Daynes <jdaynes@haventreebank.com>
2026-01-28 14:22:49 -0400

    Added Config Implimentation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs;
+use std::process;
+
+struct Config {
+    query: String,
+    file_path: String,
 }
+
+impl Config {
+    fn build(&args: Vec<String>) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        };
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Ok(Config{query, file_path})
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    let contents = fs::read_to_string(config.file_path).expect("Something went wrong reading the file");
+    println!("With text:\n{contents}");
+}
+
